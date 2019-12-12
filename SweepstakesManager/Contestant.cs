9406f2e140f0166d2790088216a4ed3dc57230ca
Elias Holter <eliasholter1@gmail.com>
2019-12-12 10:07:20 -0600

    Initial commit, basic Contestant class set up

---
+++ SweepstakesManager/Contestant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesManager
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private int registrationNumber;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return LastName;
+            }
+            set
+            {
+                LastName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
---
+++ SweepstakesManager/UserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesManager
+{
+    public static class UserInterface
+    {
+        public static string GetCustomerFirstName()
+        {
+            Console.WriteLine("Please enter your first name");
+            return Console.ReadLine();
+        }
+
+        public static string GetCustomerLastName()
+        {
+            Console.WriteLine("Please enter your last name");
+            return Console.ReadLine();
+        }
+
+
+        public static 
+    }
+}
